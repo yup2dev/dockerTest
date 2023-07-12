67eaeea5168081099f8851ebfab184c514c10247
sangyeopK <creativethony@naver.com>
2023-07-12 12:05:15 +0900

    first commit

---
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -0,0 +1,14 @@
+package com.ll.basic1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String showMain(){
+        return "<h1>hello</h1>";
+    }
+}
